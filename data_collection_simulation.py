a1c644f7b7140b21339d3cb435e6055e81fb9d75
MinchalaVenkatSunil <137503198+MinchalaVenkatSunil@users.noreply.github.com>
2023-11-24 13:30:46 +0530

    Update data_collection_simulation.py

--- data_collection_simulation.py
+++ data_collection_simulation.py
@@ -1,51 +1,27 @@
-# data_collection_simulation.py
-
 import os
-import shutil
-import pandas as pd
 from google.cloud import storage
 
-def simulate_weekly_data_collection(csv_file_path, destination_path, segment_size=100, bucket_name="dcase2023dataset"):
-    # Ensure the destination directory exists
-    os.makedirs(destination_path, exist_ok=True)
-
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv(csv_file_path)
-
-    # Shuffle the DataFrame for randomness
-    df = df.sample(frac=1).reset_index(drop=True)
-
-    # Set the path to the service account key file
-    key_path = "/app/newtestproject-405920-4be1a3498a32.json"
-
+def upload_csv_to_gcs(csv_file_path, bucket_name, destination_blob_name, key_path):
     # Create a Google Cloud Storage client with explicit credentials
     client = storage.Client.from_service_account_json(key_path)
 
-    # Simulate weekly data collection and upload to GCS
-    for i in range(0, len(df), segment_size):
-        segment = df.iloc[i:i + segment_size]
-
-            # Upload the file to Google Cloud Storage
-            upload_to_gcs(client, bucket_name, destination_file_path)
-
-def upload_to_gcs(client, bucket_name, file_path):
-    """Upload a file to Google Cloud Storage."""
+    # Upload the CSV file to Google Cloud Storage
     bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(os.path.basename(file_path))
-    blob.upload_from_filename(file_path)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(csv_file_path)
 
 if __name__ == "__main__":
-    # Replace with the actual path to your CSV file
+    # Replace with the actual path to your CSV file within the Docker image
     csv_file_path = "/app/attributes_00.csv"
-    
-    # Replace with the actual destination directory for simulated weekly data collection
-    destination_path = "/app/data"
-
-    # Specify the number of rows to include in each weekly segment
-    segment_size = 100
 
     # Replace with your Google Cloud Storage bucket name
     bucket_name = "dcase2023dataset"
 
-    # Run the simulation
-    simulate_weekly_data_collection(csv_file_path, destination_path, segment_size, bucket_name)
+    # Replace with the desired destination blob name in GCS
+    destination_blob_name = "data/attributes_00.csv"
+
+    # Replace with the actual path to your service account key file
+    key_path = "/app/newtestproject-405920-4be1a3498a32.json"
+
+    # Run the upload script
+    upload_csv_to_gcs(csv_file_path, bucket_name, destination_blob_name, key_path)
