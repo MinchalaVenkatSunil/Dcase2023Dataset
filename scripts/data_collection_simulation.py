ed882846fd732e8eeb10f452d2d7e59157ae7a5d
MinchalaVenkatSunil <venkatsunilminchala@gmail.com>
2023-11-27 13:47:55 +0200

    Refactoring and clean up

--- scripts/data_collection_simulation.py
+++ scripts/data_collection_simulation.py
@@ -3,50 +3,6 @@ from google.cloud import storage
 import pandas as pd
 from datetime import datetime
 
-# def simulate_weekly_data_collection_other_solution():
-#     """
-#     Simulates weekly data collection and uploads CSV files to Google Cloud Storage.
-#     """
-#     # File paths for CSV files
-#     csv_file_path_fan = "/app/fan_attributes_00.csv"
-#     csv_file_path_bearing = "/app/bearing_attributes_00.csv"
-
-#     # Google Cloud Storage configuration
-#     bucket_name = "dcase2023bucketdataset"
-    
-#     # Include year, month, and day in the folder structure
-#     current_date = datetime.now().strftime("%Y/%m/%d")
-#     destination_blob_name_fan = f"data/{current_date}/fan_attributes_00.csv"
-#     destination_blob_name_bearing = f"data/{current_date}/bearing_attributes_00.csv"
-    
-#     key_path = "/app/mldocker-4713e7f8b358.json"
-
-#     # Initialize Google Cloud Storage client
-#     client = storage.Client.from_service_account_json(key_path)
-#     bucket = client.get_bucket(bucket_name)
-
-#     # Upload CSV files to Google Cloud Storage
-#     blob_fan = bucket.blob(destination_blob_name_fan)
-#     blob_fan.upload_from_filename(csv_file_path_fan)
-
-#     blob_bearing = bucket.blob(destination_blob_name_bearing)
-#     blob_bearing.upload_from_filename(csv_file_path_bearing)
-
-#     """
-#     Simulates weekly data collection and uploads CSV files to local OS.
-#     """
-#     # Local file processing with pandas
-#     fan_data = pd.read_csv(csv_file_path_fan)
-#     bearing_data = pd.read_csv(csv_file_path_bearing)
-
-#     # Output path for saving processed data locally
-#     output_path = f'/app/weekly/upload/{current_date}/'
-#     os.makedirs(output_path, exist_ok=True)
-
-#     # Save the processed data locally for weekly upload
-#     fan_data.to_csv(os.path.join(output_path, 'fan_data.csv'), index=False)
-#     bearing_data.to_csv(os.path.join(output_path, 'bearing_data.csv'), index=False)
-
 def download_data_from_gcs(bucket_name, source_blob_name, destination_file_path, key_path):
     """Download a file from Google Cloud Storage."""
     client = storage.Client.from_service_account_json(key_path)
@@ -57,7 +13,7 @@ def download_data_from_gcs(bucket_name, source_blob_name, destination_file_path,
 def simulate_weekly_data_collection():
     """
     Assuming the source data will be available weekly under the secured GCP bucket "dcase2023bucketdataset".
-    Or we can also assume and work with any server
+    Or we can also assume and work with any server or from a url and extract the zip directly
     Simulates weekly data collection and downloads CSV files from Google Cloud Storage.
     """
 
--- scripts/data_transfer_to_gcs.py
+++ scripts/data_transfer_to_gcs.py
@@ -1,59 +1,3 @@
-# import os
-# from google.cloud import storage
-# from datetime import datetime
-
-# def upload_data_to_gcs(bucket_name, source_folder, key_path):
-#     """Upload data to Google Cloud Storage."""
-#     print("=== Starting upload_data_to_gcs ===")
-
-#     print(f"Current working directory: {os.getcwd()}")
-#     print(f"Current working directory: {os.path.pardir}")
-
-#     client = storage.Client.from_service_account_json(key_path)
-#     bucket = client.get_bucket(bucket_name)
-
-#     # Include year, month, and day in the folder structure
-#     #current_date = datetime.now().strftime("%Y/%m/%d")
-#     destination_folder = f'/data/weekly/upload/'
-#     # destination_folder = f'data/{current_date}/weekly_upload/'
-
-#     print(f"Does the folder exist: {source_folder}")
-
-#     # List all files in the source folder
-#     if os.path.exists(source_folder):
-#         files = [f for f in os.listdir(source_folder) if os.path.isfile(os.path.join(source_folder, f))]
-
-#         # Upload each file to Google Cloud Storage
-#         for file_name in files:
-#             source_file_path = os.path.join(source_folder, file_name)
-#             destination_blob_name = os.path.join(destination_folder, file_name)
-#             blob = bucket.blob(destination_blob_name)
-#             blob.upload_from_filename(source_file_path)
-#             print(f"Uploaded {file_name} to Google Cloud Storage at {destination_blob_name}")
-
-#     else:
-#         print(f"The directory '{source_folder}' does not exist.")
-
-#     print("=== Finished upload_data_to_gcs ===")
-
-# def main():
-#     print("=== Starting main ===")
-
-#     # Google Cloud Storage configuration
-#     bucket_name = os.environ.get("GCS_BUCKET_NAME", "dcase2023bucketdataset")
-#     key_path = os.environ.get("GCS_KEY_PATH", "/app/mldocker-4713e7f8b358.json")
-
-#     # Source folder containing the data to be uploaded
-#     source_folder = 'data/weekly/upload'
-
-#     # Upload data to Google Cloud Storage
-#     upload_data_to_gcs(bucket_name, source_folder, key_path)
-
-#     print("=== Finished main ===")
-
-# if __name__ == "__main__":
-#     main()
-
 import os
 from google.cloud import storage
 
@@ -67,9 +11,6 @@ def upload_to_gcs(local_file_path, bucket_name, destination_blob_name, key_path)
 
 def transfer_data_to_gcs(local_data_folder, bucket_name, key_path):
     """Transfer locally collected data to Google Cloud Storage."""
-    print("---------------------------------------------------")
-    print(f"Current Working Directory: {os.getcwd()}")
-    print(f"Script Directory: {os.path.dirname(os.path.realpath(__file__))}")
     print(f"Starting data transfer from {local_data_folder} to {bucket_name}...")
 
     # Check if the specified path exists
@@ -109,12 +50,9 @@ def transfer_data_to_gcs(local_data_folder, bucket_name, key_path):
             # Upload the file to Google Cloud Storage
             upload_to_gcs(local_file_path, bucket_name, destination_blob_name, key_path)
 
-
     print("Data transfer completed.")
-    print("---------------------------------------------------")
 
 if __name__ == "__main__":
-    print("---------------------------------------------------")
     print("Starting main...")
     
     # Set configuration details
@@ -124,7 +62,4 @@ if __name__ == "__main__":
 
     # Transfer data to Google Cloud Storage
     transfer_data_to_gcs(local_data_folder, bucket_name, key_path)
-    
-    print("Main completed.")
-    print("---------------------------------------------------")
 
